f6b73cc91e1eb42e23d16eb30d1103760f878cc7
Gav Hanna <gavhanna@gmail.com>
2018-04-11 14:19:44 +0000

    devise user auth working

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     root 'static_pages#home'
     get '/about', to: 'static_pages#about'
     get '/products', to: 'products#index'
+    
+    root :to => "site#home"
 
 end
