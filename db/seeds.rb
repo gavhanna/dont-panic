16aad54b9cbe45c75fd9819a76ad31788bd3d597
Gav Hanna <gavhanna@gmail.com>
2018-04-21 10:58:16 +0000

    added more db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,27 @@ data = [
         image_url: "pangalactic.JPG",
         price: 42,
         category: "Food and Drink"
+    },
+    {
+        title: "Towel",
+        description: "A towel is just about the most massively useful thing any interstellar Hitchhiker can carry. Partly it has great practical value. You can wrap it around you for warmth as you bound across the cold moons of Jaglan Beta; you can lie on it on the brilliant marble-sanded beaches of Santraginus V, inhaling the heady sea vapours; you can sleep under it beneath the stars which shine so redly on the desert world of Kakrafoon; use it to sail a miniraft down the slow heavy River Moth; wet it for use in hand-to-hand combat; wrap it round your head to ward off noxious fumes or avoid the gaze of the Ravenous Bugblatter Beast of Traal (a mind-bogglingly stupid animal, it assumes that if you can't see it, it can't see you — daft as a brush, but very very ravenous); you can wave your towel in emergencies as a distress signal, and of course you can dry yourself off with it if it still seems to be clean enough.",
+        image_url: "towel.JPG",
+        price: 42,
+        category: "Essentials"
+    },
+    {
+        title: "The Hitchhiker's Guide to the Galaxy (travel guide)",
+        description: "The Hitchhiker's Guide to the Galaxy is a wholly remarkable book. Perhaps the most remarkable, certainly the most successful book ever to come out of the great publishing corporations of Ursa Minor - of which no Earthman had ever heard of. More popular than The Celestial Home Care Omnibus, better selling than Fifty-three More Things to do in Zero Gravity, and more controversial than Oolon Colluphid's trilogy of philosophical blockbusters Where God Went Wrong, Some More of God's Greatest Mistakes and Who is this God Person Anyway? It's already supplanted the Encyclopedia Galactica as the standard repository of all knowledge and wisdom, for two important reasons. First, it's slightly cheaper; and secondly it has the words DON'T PANIC printed in large friendly letters on its cover. - The Guide",
+        image_url: "theguide.JPG",
+        price: 420,
+        category: "Essentials"
+    },
+    {
+        title: "Nutri-Matic Drink Dispenser",
+        description: "The Nutri-Matic Drink Dispenser (NMDD) is a machine built by the Sirius Cybernetics Corporation. The NMDD is designed to sense, by various complicated means, exactly what kind of beverage will be most pleasing to the person using the machine. Why it does this is unknown, because it then always produces something \"almost, but not quite entirely unlike tea.\" There was a NMDS on the Heart of Gold, which did manage, at one point and entirely by accident, to make a perfect cup of tea.",
+        image_url: "tea.JPG",
+        price: 420,
+        category: "Food and Drink"
     }
 ]
 
